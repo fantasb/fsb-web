3db86df0d6034b4e16b57b89b51c15a3d2fce6c2
alec <alec.hulce@gmail.com>
2016-04-02 02:29:39 -0700

    Allow gulp to run if server inited outside repo root

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ if(process.env && !process.env.NODE_ENV) {
   process.env.NODE_ENV = 'development';
 }
 
+process.chdir(__dirname); // so index.js can be called outside repo root and process.cwd() still functions
 module.exports = process.env.NOCLUSTER
 	? require('./app/server.js')
 	: require('./app/cluster.js')
