d6c762d1c52b42bd2791b804acacaa18669f1df3
alec <alec.hulce@gmail.com>
2016-04-25 16:29:01 -0700

    ૮( ᵒ̌ૢཪᵒ̌ૢ )ა

--- hack_restart.sh
+++ hack_restart.sh
@@ -2,9 +2,13 @@
 # Note!: This is a temp hack for me. Do not use in real scripts
 #
 
+
 killall node
 killall gulp
-#export NODE_ENV=production
-export NODE_ENV=
+if [ "$1" == "-r" ];
+	export NODE_ENV=
+else
+	export NODE_ENV=production
+fi
 /root/sire/bin/angel.sh "/var/www/fsb-demo/algo/server.js" >> /var/log/angel.log 2>&1
 /root/sire/bin/angel.sh "/var/www/ranktt-web/index.js" >> /var/log/angel.log 2>&1
