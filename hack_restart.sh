3103de1fc2d4160820309494ba0bbb063eb793dd
alec <alec.hulce@gmail.com>
2016-05-02 14:50:57 -0700

    temp hack to run postinstall in hack_restart.sh

--- hack_restart.sh
+++ hack_restart.sh
@@ -12,5 +12,6 @@ else
 fi
 cd /var/www/ranktt-web
 npm install
+npm run postinstall # temporary hack cuz for some reason `npm install` user doesnt run postinstall as parent user - #hack 
 /root/sire/bin/angel.sh "/var/www/fsb-demo/algo/server.js" >> /var/log/angel.log 2>&1
 /root/sire/bin/angel.sh "/var/www/ranktt-web/index.js" >> /var/log/angel.log 2>&1
